df1ad75e8298cbb24db4aad52bbf2a38d7eb552d
Amandeep Kaur <amandeepkaur@Amandeeps-MacBook-Air.local>
2024-03-03 13:42:18 +0530

    main

--- backend/db.js
+++ backend/db.js
@@ -8,7 +8,7 @@ mongoose.set('strictQuery', true);
 
 app.use(cors(
     {
-        origin : [],
+        origin : ["https://foodie-api-nu.vercel.app"],
         methods: ['POST','GET'],
         credentials: true
     }
