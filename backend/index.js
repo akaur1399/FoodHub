f9f92a476610bf20498f84b9120a9f59882b274f
Amandeep Kaur <amandeepkaur@Amandeeps-MacBook-Air.local>
2024-03-05 20:18:26 +0530

    .env

--- backend/index.js
+++ backend/index.js
@@ -9,19 +9,19 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.use((req,res,next)=>{
+/*app.use((req,res,next)=>{
   res.setHeader("Access-Control-Allow-Origin","http://localhost:3000");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
   );
   next();
-})
+})*/
 
 app.use(express.json());
-app.use('/',require("./Routes/CreateUser"));
-app.use('/',require("./Routes/DisplayData"));
-app.use('/',require("./Routes/OrderData"));
+app.use('/api',require("./Routes/CreateUser"));
+app.use('/api',require("./Routes/DisplayData"));
+app.use('/post',require("./Routes/OrderData"));
 
 
 const PORT = process.env.PORT
