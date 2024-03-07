4363ee41a5fbba8e5c049888dfc027fdda8ded27
Amandeep Kaur <amandeepkaur@Amandeeps-MacBook-Air.local>
2024-03-07 10:31:24 +0530

    get

--- backend/Routes/OrderData.js
+++ backend/Routes/OrderData.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 const Order = require("../models/Orders")
 
-router.get('/orderData', async (req, res) => {
+router.post('/orderData', async (req, res) => {
     let data = req.body.order_data
     await data.splice(0, 0, { Order_date: req.body.order_date })
 
