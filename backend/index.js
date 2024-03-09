97e85ae9aecee05edb52a1a12c5cacea24409821
Amandeep Kaur <amandeepkaur@Amandeeps-MacBook-Air.local>
2024-03-09 10:02:44 +0530

    access control

--- backend/index.js
+++ backend/index.js
@@ -9,14 +9,14 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-/*app.use((req,res,next)=>{
-  res.setHeader("Access-Control-Allow-Origin","http://localhost:3000");
+app.use((req,res,next)=>{
+  res.setHeader("Access-Control-Allow-Origin","https://65e94e8f5a96b3a82bf1c94e--lovely-vacherin-6a1458.netlify.app");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
   );
   next();
-})*/
+})
 
 app.use(express.json());
 app.use('/api',require("./Routes/CreateUser"));
