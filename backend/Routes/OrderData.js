63d64ccb8d303d63474358b8042717fc06f60771
Amandeep Kaur <amandeepkaur@Amandeeps-MacBook-Air.local>
2024-03-09 09:45:10 +0530

    orderdata

--- backend/Routes/OrderData.js
+++ backend/Routes/OrderData.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 const Order = require("../models/Orders")
 
-router.post('/orderData', async (req, res) => {
+router.get('/orderData', async (req, res) => {
     let data = req.body.order_data
     await data.splice(0, 0, { Order_date: req.body.order_date })
 
@@ -41,7 +41,7 @@ router.post('/orderData', async (req, res) => {
     }
 })
 
-router.post('/myorderData', async(req,res)=>{
+router.get('/myorderData', async(req,res)=>{
     try {
         let myData = await Order.findOne({'email':req.body.email})
         res.json({orderData:myData})
