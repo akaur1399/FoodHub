b0636925827571b62e38ea384243c7397b997c13
Amandeep Kaur <amandeepkaur@Amandeeps-MacBook-Air.local>
2024-03-09 12:10:54 +0530

    cors

--- backend/db.js
+++ backend/db.js
@@ -10,11 +10,11 @@ const app = express()
 mongoose.set('strictQuery', true);
 
 app.use(cors(
-    {
+    /*{
         origin : ['https://65e94e8f5a96b3a82bf1c94e--lovely-vacherin-6a1458.netlify.app'],
         methods: ['POST','GET'],
         credentials: true
-    }
+    }*/
 ));
             
 
